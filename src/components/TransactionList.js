02c9ba47c67ed47f80dc18ad6427fe45f9f6fd3d
Afnan Mk <afnanmalik80@gmail.com>
2020-12-02 23:53:09 +0300

    modified TransactionList file

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,34 +1,38 @@
-import React, { useContext } from "react";
-import { GlobalContext } from "../context/GlobalState";
+import React, { useContext } from "react"
+import { GlobalContext } from "../context/GlobalState"
 
 function TransactionList() {
-  const { transactions, deleteTransaction } = useContext(GlobalContext);
+  const { transactions, deleteTransaction } = useContext(GlobalContext)
 
   return (
     <div>
       <h3>History</h3>
       <ul id="list" className="list">
-        {transactions.map(transaction => (
-          <li
-            key={transaction.id}
-            className={transaction.amount < 0 ? "minus" : "plus"}
-          >
-            {transaction.text}
-            <span>
-              {transaction.amount < 0 ? "-" : "+"}$
-              {Math.abs(transaction.amount)}
-            </span>
-            <button
-              className="delete-btn"
-              onClick={() => deleteTransaction(transaction.id)}
+        {transactions.length ? (
+          transactions.map(transaction => (
+            <li
+              key={transaction.id}
+              className={transaction.amount < 0 ? "minus" : "plus"}
             >
-              x
-            </button>
-          </li>
-        ))}
+              {transaction.text}
+              <span>
+                {transaction.amount < 0 ? "-" : "+"}$
+                {Math.abs(transaction.amount)}
+              </span>
+              <button
+                className="delete-btn"
+                onClick={() => deleteTransaction(transaction.id)}
+              >
+                x
+              </button>
+            </li>
+          ))
+        ) : (
+          <h4 style={{ color: "gray" }}>No transactions yet</h4>
+        )}
       </ul>
     </div>
-  );
+  )
 }
 
-export default TransactionList;
+export default TransactionList
